3593faddc7f5940851d902a709a19bb7d0d77902
Artur <arturlee_word@hotmail.com>
2019-10-16 01:47:33 +0200

    React

---
+++ src/Components/Main.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import Title from './Title'
+import Instagram from './Instagram'
+import AddPhoto from './AddPhoto'
+import { Route } from 'react-router-dom'
+class Main extends Component {
+    constructor() {
+        super()
+        this.state = {
+            posts: [{
+                id: 0,
+                description: "halo ",
+                imageLink: "https://cdn.pixabay.com/photo/2015/05/31/15/07/business-792113_1280.jpg"
+            }]
+        }
+        this.removePhoto = this.removePhoto.bind(this)
+    }
+    removePhoto(postRemoved) {
+        console.log(postRemoved.description)
+        this.setState((state) => ({
+            posts: state.posts.filter(post => post !== postRemoved)
+        }))
+    }
+    addPhoto(postSubmitted) {
+        this.setState(state => ({
+            posts: state.posts.concat([postSubmitted])
+        }))
+    }
+    componentDidMount() {
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log(prevState.posts)
+        console.log(this.state)
+    }
+    render() {
+        console.log(this.state.posts)
+        return (
+
+            <div>
+                <Route exact path="/" render={() => (
+                    <div>
+                        <Title title={'Instagram'} />
+                        <Instagram posts={this.state.posts} onRemovePhoto={this.removePhoto} onNavigate={this.navigate} />
+                    </div>
+                )} />
+                <Route path="/AddPhoto" render={({ history }) => (
+                    <AddPhoto onAddPhoto={(addedPost) => {
+                        this.addPhoto(addedPost)
+                        history.push('/')
+                    }} />
+                )} />
+            </div>
+        )
+    }
+}
+export default Main
---
+++ src/Components/photo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+function Photo(props) {
+    const post = props.post
+    return <figure className="figure">
+        <img className="photo" src={post.imageLink} alt={post.description} />
+        <div className="description">
+        <figcaption> <p> {post.description} </p> </figcaption>
+        </div>
+        <div className="button-container">
+            <button onClick={() => {
+                props.onRemovePhoto(post)
+            }}> Remove </button>
+        </div>
+    </figure>
+}
+Photo.propTypes = {
+    post: PropTypes.object.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
+}
+export default Photo
